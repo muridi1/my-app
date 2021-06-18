1205c3e1769dee321f9529437bb3611731522bcc
mohamed <muridi51@gmail.com>
2021-06-18 12:00:34 +0100

    my first commit

---
+++ src/Components/Places.tsx
@@ -0,0 +1,23 @@
+interface PlacesProps {
+    title: string;
+    placeName: string;
+    countryName: string;
+    image: string;
+    mapLink: string;
+    text: string;
+  }
+  
+  function Places (props: PlacesProps): JSX.Element {
+    return (
+      <section>
+        <h2>{props.title}</h2>
+        <h3>{props.placeName}, {props.countryName}</h3>
+        <p>
+          {props.text}
+        </p>
+      </section>
+    );
+  }
+  
+  export default Places;
+  
