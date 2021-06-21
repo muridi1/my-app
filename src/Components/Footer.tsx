e43d04ea283cd2a8290df33c632e316d9badb11e
mohamed <muridi51@gmail.com>
2021-06-21 12:24:05 +0100

    added itallics and bold letters

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer(): JSX.Element {
     return (
       <header>
-        <h3>THE END</h3>
-        <p>Thank you for getting this far!</p>
+        <h3><em>THE END</em></h3>
+        <p>Thank you for getting<strong> this </strong>Thank you for getting far!</p>
       </header>
     );
   }
