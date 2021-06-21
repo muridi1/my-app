94ddfa1d4ae5fb3674ff7c757668e6ccb93bfd4c
mohamed <muridi51@gmail.com>
2021-06-21 10:20:41 +0100

    removing logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import MainContent from "./Components/MainContent"
 import Header from "./Components/Header"
