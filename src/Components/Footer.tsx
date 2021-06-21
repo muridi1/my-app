85f25f756abe2d2e684390fc70b61bb19a0e666c
mohamed <muridi51@gmail.com>
2021-06-21 10:09:01 +0100

    testing something

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer(): JSX.Element {
     return (
       <header>
         <h3>THE END</h3>
-        <p>Thank you for getting this far.</p>
+        <p>Thank you for getting this far!</p>
       </header>
     );
   }
